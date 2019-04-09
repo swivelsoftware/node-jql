63603715dcc7605752a1220f1c5994f5e84a97af
Kennys Ng <kennysng@hotmail.com.hk>
2019-04-09 16:08:08 +0800

    modify the list of types supported

--- src/Sql.ts
+++ src/Sql.ts
@@ -1,20 +1,45 @@
+import { diff } from 'deep-diff'
+import isRegexp = require('is-regexp')
 import squel = require('squel')
 
-export type Type = 'any'|'string'|'number'|'boolean'|'object'|'symbol'|'Date'|'RegExp'
+export type Type = 'any'|'string'|'number'|'boolean'|'object'|'Date'|'RegExp'
 
+/**
+ * Get type of value
+ * @param value [any]
+ */
 export function getType(value: any): Type {
   if (value instanceof Date) return 'Date'
-  if (value instanceof RegExp) return 'RegExp'
+  if (isRegexp(value)) return 'RegExp'
   const type = typeof value
   switch (type) {
     case 'string':
     case 'number':
     case 'boolean':
     case 'object':
-    case 'symbol':
       return type
     default:
       return 'any'
+  }
+}
+
+/**
+ * Compare 2 values for equality
+ * @param l [T]
+ * @param r [T]
+ */
+export function equals<T>(l: T, r: T): boolean {
+  if (l instanceof Date && r instanceof Date) {
+    return l.getTime() === r.getTime()
+  }
+  else if (isRegexp(l) && isRegexp(r)) {
+    return String(l) === String(r)
+  }
+  else if (typeof l === 'object' && typeof r === 'object') {
+    return !!diff(l, r)
+  }
+  else {
+    return l === r
   }
 }
 
