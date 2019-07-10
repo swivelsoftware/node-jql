10790a51609c1ec12e69d6bb902e7bd4c3e7f068
Kennys Ng <kennysng@hotmail.com.hk>
2019-07-10 15:36:09 +0800

    support user-defined aggregate function

--- src/jql/create/function.ts
+++ src/jql/create/function.ts
@@ -28,6 +28,11 @@ function parseFunction(code: string): Function {
  */
 export interface ICreateFunctionJQL extends IJQL, IParseable {
   /**
+   * Whether it is an aggregate function
+   */
+  aggregate?: boolean
+
+  /**
    * Function name
    */
   name: string
@@ -53,6 +58,7 @@ export interface ICreateFunctionJQL extends IJQL, IParseable {
  */
 export class CreateFunctionJQL extends JQL implements ICreateFunctionJQL {
   public readonly classname = CreateFunctionJQL.name
+  public aggregate?: boolean
   public name: string
   public fn: Function
   public parameters: Type[] = []
@@ -71,17 +77,34 @@ export class CreateFunctionJQL extends JQL implements ICreateFunctionJQL {
    */
   constructor(name: string, fn: string|Function, type?: Type, ...parameters: Type[])
 
+  /**
+   * @param aggregate [boolean]
+   * @param name [string]
+   * @param fn [string|Function]
+   * @param type [Type] optional
+   * @param parameters [Array<Type>] optional
+   */
+  constructor(aggregate: true, fn: string|Function, type?: Type, ...parameters: Type[])
+
   constructor(...args: any[]) {
     super()
 
     // parse args
-    let name: string, fn: Function, type: Type = 'any', parameters: Type[] = []
+    let aggregate: boolean|undefined, name: string, fn: Function, type: Type = 'any', parameters: Type[] = []
     if (typeof args[0] === 'object') {
       const json = args[0] as ICreateFunctionJQL
+      aggregate = json.aggregate
       name = json.name
       fn = typeof json.fn === 'string' ? parseFunction(json.fn) : json.fn
       if (json.type) type = json.type
       if (json.parameters) parameters = json.parameters
+    }
+    else if (typeof args[0] === 'boolean') {
+      aggregate = args[0]
+      name = args[1]
+      fn = typeof args[2] === 'string' ? parseFunction(args[2]) : args[2]
+      type = args[3] || type
+      parameters = args.slice(4)
     }
     else {
       name = args[0]
@@ -90,7 +113,11 @@ export class CreateFunctionJQL extends JQL implements ICreateFunctionJQL {
       parameters = args.slice(3)
     }
 
+    // check args
+    if (aggregate && this.parameters.length > 1) throw new SyntaxError('Aggregate function supports only 1 parameter')
+
     // set args
+    this.aggregate = aggregate
     this.name = name
     this.fn = fn
     this.type = type
@@ -108,6 +135,7 @@ export class CreateFunctionJQL extends JQL implements ICreateFunctionJQL {
   // @override
   public toJson(): ICreateFunctionJQL {
     const result = { name: this.name, fn: this.fn.toString() } as ICreateFunctionJQL
+    if (this.aggregate) result.aggregate = this.aggregate
     if (this.type !== 'any') result.type = this.type
     if (this.parameters.length) result.parameters = this.parameters
     return result
