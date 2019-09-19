3091a297bf39c79c1c01efb8d0043efc303c4551
Kennys Ng <kennysng@hotmail.com.hk>
2019-09-19 09:55:31 +0800

    reset project for v3

--- script/post-process.ts
+++
@@ -1,9 +0,0 @@
-export default function process(content: string[]): string[] {
-  const oldIndex = content.findIndex(line => line.startsWith('export abstract class Expression')) - 5
-  const newIndex = content.findIndex(line => line.startsWith('export class Unknown extends Expression')) - 5
-  const subContent = content.splice(oldIndex, 23)
-  for (let i = 0, length = subContent.length; i < length; i += 1) {
-    content.splice(newIndex + i, 0, subContent[i])
-  }
-  return content
-}
